b9a75127a3592c4e07ad923c07edcc0712923fd0
IbnuSabil <ibnusabil2301@gmail.com>
2023-11-20 11:38:53 +0800

    Program Feed Back

---
+++ Project/FeedbackUser/build.sh
@@ -0,0 +1,1 @@
+g++ feedBack.cpp -o run
--- Project/FeedbackUser/feedBack.cpp
+++ Project/FeedbackUser/feedBack.cpp
@@ -1,7 +1,68 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main (){
-    
+struct feedBack
+{
+    string nickName;
+    string comment;
+};
+
+int main()
+{
+
+    feedBack user;
+    bool validasiNickName = false;
+    bool validasiComment = false;
+    int countFeedBack = 0;
+// Input Nick name user
+inputNickName:
+    cout << "NickName Anda : ";
+    getline(cin,user.nickName);
+    // Validasi input
+    cout << user.nickName.length() << endl;
+    if (user.nickName.length() >= 15)
+    {
+        cout << "Nick name anda melebihi limit" << endl;
+        cout << endl;
+        goto inputNickName;
+    }
+    else
+    {
+        validasiNickName = true;
+    }
+
+// Input COMMENT
+inputComment:
+    cout << "FeedBack Anda : ";
+    getline(cin,user.comment);
+    // Validasi input comment
+    if (user.comment.length() > 1000)
+    {
+        cout << "Feed back anda melebihi limit : ";
+        goto inputComment;
+    }
+    else
+
+    {
+        validasiComment = true;
+    }
+
+    ofstream data;
+    if (validasiNickName && validasiComment)
+    {
+        countFeedBack++;
+        data.open("dataBaseFeedback", ios::app);
+
+        data << "--------------------------------------------------------" <<endl;
+        data << "Nick Name:" << user.nickName <<endl;
+        data << "Feed Back: " << user.comment <<endl;
+        data << "Feed back ke-" << countFeedBack <<endl;
+
+        data << "--------------------------------------------------------" <<endl;
+        data.close();
+
+    }
+
     return 0;
 }
